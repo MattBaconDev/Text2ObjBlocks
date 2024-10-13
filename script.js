bf5e91ba125b308203f83f4d26eaf5a9c03a12cb
Matt Bacon <mattbacondev@outlook.com>
2024-10-13 14:30:48 +0100

    Reduced gap between letters

--- script.js
+++ script.js
@@ -190,7 +190,7 @@ class App {
 			const xBounds = { left: lPos.x, right: rPos.x };
 			if (i > 0) {
 				const dist = xBounds.left - prevXBounds.right;
-				const shift = (-dist) + Math.max(3.5, ((cfg.fontSize/3) * cfg.letterSpacing));
+				const shift = (-dist) + Math.max(2.5, ((cfg.fontSize/4) * cfg.letterSpacing));
 				child.translateX(shifted + shift);
 				shifted += shift;
 			}
