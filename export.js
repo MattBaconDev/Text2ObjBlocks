76fa619977fd6576e7c5e1ef21e4fd667b2af4b1
Matt Bacon <mattbacondev@outlook.com>
2024-10-13 21:34:51 +0100

    Export - Re-simplified now that the camera moves rather than the objects themselves

--- export.js
+++ export.js
@@ -3,19 +3,8 @@ import { STLExporter } from 'three/addons/exporters/STLExporter.js';
 export function enableExportScene(button, app) {
 	button.addEventListener('click', async () => {
 		const exporter = new STLExporter();
-		const oldRot = app.scene.rotation.clone();
-		const oldPos = app.scene.position.clone();
-		app.scene.rotation.set(0, 0, 0);
-		app.scene.position.set(0, 0, 0);
-		await app.render();
-		setTimeout(() => {
-			const data = exporter.parse(app.scene);
-			downloadFile(data, app.getSceneName());
-			setTimeout(() => {
-				app.scene.rotation.set(oldRot.x, oldRot.y, oldRot.z);
-				app.scene.position.set(oldPos.x, oldPos.y, oldPos.z);
-			}, 50);
-		}, 50);
+		const data = exporter.parse(app.svgGroup);
+		downloadFile(data, app.getSceneName());
 	});
 }
 
