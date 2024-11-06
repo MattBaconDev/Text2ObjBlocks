befd63055d74c5558d6dcbbb1d251eb8b25bf54a
Matt Bacon <mattbacondev@outlook.com>
2024-11-06 21:53:57 +0000

    Export func can now be called directly

--- export.js
+++ export.js
@@ -2,10 +2,14 @@ import { STLExporter } from 'three/addons/exporters/STLExporter.js';
 
 export function enableExportScene(button, app) {
 	button.addEventListener('click', async () => {
-		const exporter = new STLExporter();
-		const data = exporter.parse(app.svgGroup);
-		downloadFile(data, app.getSceneName());
+		exportScene(app);
 	});
+}
+
+export function exportScene(app) {
+	const exporter = new STLExporter();
+	const data = exporter.parse(app.svgGroup);
+	downloadFile(data, app.getSceneName());
 }
 
 function downloadFile(data, name) {
